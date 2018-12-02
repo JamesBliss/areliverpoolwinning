1360145e0ec96f65e6a83104de9c93d00ba7580e
James Bliss <james.bliss@lush.co.uk>
2018-12-02 15:41:18 +0000

    Attempt to add favicon

--- components/Meta.js
+++ components/Meta.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 const Meta = () => (
   <Head>
     <title>Are liverpool winnning?</title>
+    <link rel="shortcut icon" href="/public/favicon.ico"></link>
+    <link rel="shortcut icon" href="https://res.cloudinary.com/jamesbliss/image/upload/v1543765247/favicon_diglsp.ico"></link>
   </Head>
 );
 
