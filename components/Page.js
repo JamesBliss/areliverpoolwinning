b4cd1057c3f8baea617974484c6e714e39843bd2
James Bliss <james.bliss@lush.co.uk>
2018-12-07 11:27:48 +0000

    Adding google tag manager

--- components/Page.js
+++ components/Page.js
@@ -3,6 +3,9 @@ import React from 'react';
 // components
 import GlobalStyles from './GlobalStyles';
 import Meta from './Meta';
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 class Page extends React.Component {
   render() {
