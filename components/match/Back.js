f95c4f2107e0550f557002a4550c7c8b0d77260a
James Bliss <thejamesbliss@gmail.com>
2019-02-24 12:27:29 +0000

    changing back button behaviour

--- components/match/Back.js
+++ components/match/Back.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import Pressure from 'react-pressure';
 import Router from 'next/router'
 
 // styles
 import { Button, Time } from './BackStyles';
 
 // comp
-const Back = ({ force, minute, status }) => {
-  if (force >= 1) {
-    Router.push('/');
-  }
-
+const Back = ({ status }) => {
   return (
-    <Button force={ force }>
+    <Button onClick={ Router.push('/') }>
       <svg class="long-arrow-left sc-bxivhb hfbjfb" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
         <path fill="currentColor" d="M30,13.4v3.3H6.4l5.9,6L10,25L0,15L10,5l2.3,2.3l-5.9,6H30z"></path>
       </svg>
@@ -30,4 +25,4 @@ const Back = ({ force, minute, status }) => {
   );
 };
 
-export default Pressure(Back);
+export default Back;
