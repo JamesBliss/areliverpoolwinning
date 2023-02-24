0fe2e057cbe0f21b8f16bfb754840c17a68b91f9
James Bliss <james.bliss@lush.co.uk>
2023-02-24 18:46:25 +0000

    tweak

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import Router from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 import { FC, ReactNode } from "react";
 import type { AppProps } from "next/app";
-import NProgress from 'nprogress';
+import NProgress from "nprogress";
 
 //
 import "../global.css";
 
 // components
 import Head from "@components/Meta.Head";
-import Page from '@components/Layout.Page';
 import AnalyticsWrapper from "@components/Meta.Analytics";
-
-//
-Router.onRouteChangeStart = () => {
-  NProgress.start();
-};
-Router.onRouteChangeComplete = () => {
-  NProgress.done();
-  window.scrollTo(0, 0);
-};
-Router.onRouteChangeError = () => {
-  NProgress.done();
-};
 
 //
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>;
 
-
 // export
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      window.scrollTo(0, 0);
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   const Layout = (Component as any).Layout || Noop;
 
   return (
@@ -41,4 +51,4 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       </Layout>
     </>
   );
-};
+}
